611117f9c41c9be5c9289bb7d578789125f2a575
Michael <michd.weaver@gmail.com>
2017-12-27 22:10:50 -0600

    fixed bugs in js

--- horizontal/horiz.js
+++ horizontal/horiz.js
@@ -1,44 +1,42 @@
+function showButtons() {
+  if ($('.pic').first().hasClass('active')) {
+    $('.prev').addClass('hidden');
+  } else if ($('.pic').last().hasClass('active')) {
+    $('.next').addClass('hidden');
+  } else {
+    $('.button').removeClass('hidden');
+  }
+}
+
+function nextImg() {
+  var active = $('.active');
+  var next = active.next();
+  active.removeClass('active');
+  next.addClass('active');
+  showButtons();
+}
+
+function prevImg() {
+  var active = $('.active');
+  var prev = active.prev();
+  active.removeClass('active');
+  prev.addClass('active');
+  showButtons();
+}
+
+function toStart() {
+  $('.pic').removeClass('active');
+  $('.pic').first().addClass('active');
+  showButtons();
+}
+
+
 $(document).ready(function() {
 
-  function showButtons(){
-    if ( $('.pic').first().hasClass('active')) {
-      $('.prev').addClass('hidden');
-    }
-    else if ( $('.pic').last().hasClass('active')) {
-      $('.next').addClass('hidden');
-    }
-    else {
-      $('.button').removeClass('hidden');
-    }
-  }
-
-  function nextImg() {
-    var active = $('.pic .active');
-    var next = active.next();
-    active.removeClass('active');
-    next.addClass('active');
-    showButtons();
-  }
-
-  function prevImg() {
-    var active = $('.pic .active');
-    var prev = active.prev();
-    active.removeClass('active');
-    prev.addClass('active');
-    showButtons();
-  }
-
-  function toStart() {
-    $('.pic').removeClass('active');
-    $('.pic').first().addClass('active');
-    $('.prev').addClass('hidden');
-    $('.next').removeClass('hidden');
-  }
-
-  $('.overlay').delay(400).fadeOut();
   showButtons();
-  $('.next').click(nextImg);
-  $('.prev').click(prevImg);
-  $('#start').click(toStart);
+  $('.overlay').delay(400).fadeOut();
+  $('.next').on('click', () => nextImg());
+  $('.prev').on('click', () => prevImg());
+  $('.start').on('click', () => toStart());
 
 });
